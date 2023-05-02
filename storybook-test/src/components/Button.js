e16255ac4efd9f15b9232700ffe1705559cbfffa
Abhijeet Saraf <abhijeetsaraf251292@gmail.com>
2023-05-01 19:09:30 -0700

    Button Primary and Secondary variants for size

--- storybook-test/src/components/Button.js
+++ storybook-test/src/components/Button.js
@@ -1,7 +1,6 @@
-
 import PropTypes from "prop-types"
 
-function Button({label, backgroundColor = "blue", size = "md", onClick}){
+function Button({label, backgroundColor = "blue", size = "md", onClick, color, border = "none"}){
     let scale =1
     if(size === "sm") scale = 0.75
     if(size === "lg") scale = 1.5
@@ -9,21 +8,44 @@ function Button({label, backgroundColor = "blue", size = "md", onClick}){
     const style = {
         backgroundColor,
         padding: `${scale*0.5}rem ${scale*1}rem`,
-        border:  "nome",
+        border,
+        borderRadius: "5px",
+        color,
     }
     return(
         <button onClick={onClick} style={style}>
             {label}
         </button>
     )
-
 }
 
-//Calling button and passing proptypes in the button
+//Calling button and passing prop types in the button
 Button.propTypes ={
     label: PropTypes.string,
     backgroundColor:PropTypes.string,
     size: PropTypes.oneOf(["sm","md","lg"]),
+    color: PropTypes.string,
+    border: PropTypes.string,
     onClick: PropTypes.func,
 }
 export default Button
+
+
+// This code defines a React functional component named Button that renders a button with a label and some additional styles based on the props passed to it.
+
+// The Button component accepts several props:
+
+// label: a required string prop that specifies the label to be displayed on the button
+// backgroundColor: an optional string prop that specifies the background color of the button (default value is "blue" if not provided)
+// size: an optional string prop that specifies the size of the button, which can be "sm" (small), "md" (medium), or "lg" (large) (default value is "md" if not provided)
+// color: an optional string prop that specifies the text color of the button
+// onClick: an optional function prop that specifies the click handler for the button
+// The Button component then creates an inline style object based on the props passed to it, which sets the background color, padding, border, border radius, and text color of the button. The scale variable is calculated based on the size prop, which is used to adjust the padding of the button.
+
+// Finally, the Button component renders a button element with the label and style properties, and attaches an onClick event handler if one is provided.
+
+// The PropTypes module is used to define the types of the props passed to the Button component. It is used to ensure that the props passed to the component have the correct type, and to provide warnings in the console if an incorrect type is passed.
+
+// The Button.propTypes object defines the prop types for the Button component, using the PropTypes module to specify the expected types of each prop. This helps to ensure that the props passed to the component are of the expected type and avoids runtime errors.
+
+// Finally, the export default Button statement exports the Button component so that it can be used in other parts of the application.
--- storybook-test/src/stories/Button.stories.js
+++ storybook-test/src/stories/Button.stories.js
@@ -7,4 +7,66 @@ export default{
 //Import the button component in the storybook page
 //Export a default object in storybook 
 
-export const Red = () => <Button label="Press me" backgroundColor = "blue"/>
+const Template = args => <Button {...args}/>
+
+
+//Exporting the object is equivalent to variant of a single design element in Figma
+
+//Primary button variants 
+//Primary token for active components to be used for primary variants
+export const primarySmall = Template.bind({})
+primarySmall.args = {
+    backgroundColor: "#3F93D3",
+    label: "Button",
+    size: "sm",
+    color: "#ffffff",
+    border: "none",
+}
+
+export const primaryMedium = Template.bind({})
+primaryMedium.args = {
+    backgroundColor: "#3F93D3",
+    label: "Button",
+    size: "md",
+    color: "#ffffff",
+    border: "none",
+}
+
+export const primaryLarge = Template.bind({})
+primaryLarge.args = {
+    backgroundColor: "#3F93D3",
+    label: "Button",
+    size: "lg",
+    color: "#ffffff",
+    border: "none",
+}
+
+//Secondary button variants
+//Secondary/Inactive token to be used for secondary variants
+
+export const secondarySmall = Template.bind({})
+secondarySmall.args = {
+    backgroundColor: "#ffffff",
+    label: "Button",
+    size: "sm",
+    color: "#3F93D3",
+    border: "solid #3f93d3",
+}
+
+export const secondaryMedium = Template.bind({})
+secondaryMedium.args = {
+    backgroundColor: "#ffffff",
+    label: "Button",
+    size: "md",
+    color: "#3F93D3",
+    border: "solid #3f93d3",
+}
+
+export const secondaryLarge = Template.bind({})
+secondaryLarge.args = {
+    backgroundColor: "#ffffff",
+    label: "Button",
+    size: "lg",
+    color: "#3F93D3",
+    border: "solid #3f93d3",
+}
